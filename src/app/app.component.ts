fcc2556553f6c3bfcd78cf839c7adffaef485bbc
arundhaj <arundhaj@gmail.com>
2020-01-30 14:54:09 +0530

    ACE editor integrated, initial styles and functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,71 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, AfterViewInit, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import * as ace from 'ace-builds';
+import { NgxSpinnerService } from 'ngx-spinner';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'rest-web-cli';
+export class AppComponent implements OnInit, AfterViewInit {
+  title = 'ng-api-cli';
+  inputUrl = 'https://jsonplaceholder.typicode.com/todos';
+
+  response: any;
+  responseBody: any;
+
+  requestBody: any;
+
+  selectedHttpMethod = 'GET';
+  httpMethods = ['GET', 'POST', 'PUT', 'DELETE'];
+
+  errorMessage = '';
+
+  loading = false;
+
+  @ViewChild('requestEditor', {static: false}) requestEditor;
+  @ViewChild('responseEditor', {static: false}) responseEditor;
+
+  constructor(
+    private http: HttpClient,
+    private spinner: NgxSpinnerService
+  ) { }
+
+  ngOnInit() {
+  }
+
+  ngAfterViewInit() {
+    this.requestEditor.setTheme('eclipse');
+    this.requestEditor.getEditor().setOptions({
+      mode: 'ace/mode/json'
+    });
+
+    this.responseEditor.setTheme('eclipse');
+    this.responseEditor.getEditor().setOptions({
+      readOnly: true,
+      mode: 'ace/mode/json'
+    });
+  }
+
+  sendRequest() {
+    this.clearPrevState();
+    this.loading = true;
+    this.spinner.show();
+    this.http.request(this.selectedHttpMethod, this.inputUrl, { observe: 'response' }).subscribe((successResponse) => {
+      this.response = successResponse;
+      this.responseBody = JSON.stringify(successResponse.body, null, 2);
+    }, (error) => {
+      this.errorMessage = error.message;
+    }).add(() => {
+      this.loading = false;
+      this.spinner.hide();
+    });
+
+  }
+
+  clearPrevState() {
+    this.errorMessage = '';
+    this.responseBody = '';
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatSelectModule } from '@angular/material/select';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatTabsModule } from '@angular/material/tabs';
+import { AceEditorModule } from 'ng2-ace-editor';
+import { NgxSpinnerModule } from 'ngx-spinner';
+
 
 @NgModule({
   declarations: [
@@ -10,7 +23,18 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    HttpClientModule,
+    FlexLayoutModule,
+    MatSelectModule,
+    MatInputModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatTabsModule,
+    AceEditorModule,
+    NgxSpinnerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
